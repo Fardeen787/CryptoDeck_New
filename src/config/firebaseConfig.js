c8a8e6fc37ef2ae8cacbb4ee6033bae731cbb865
Fardeen Khan <ffkhan@mitaoe.ac.in>
2022-07-19 10:53:25 +0530

    first commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA2pLswv0zs6zr0YYB6AW3gHNw_plZAz1I",
+  authDomain: "crypto-hunter-81022.firebaseapp.com",
+  projectId: "crypto-hunter-81022",
+  storageBucket: "crypto-hunter-81022.appspot.com",
+  messagingSenderId: "235246033036",
+  appId: "1:235246033036:web:5d0ccf5468617c1061c3a9"
+};
+
+export default firebaseConfig;
